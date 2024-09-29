148574fe2a604b4aadde823adf78a8294ce3c55a
Raheel <raheelfarouk@gmail.com>
2024-09-29 18:27:12 -0400

    added functionality to use signal name to make data packet using DBC file

--- Software/Regression Code/test_one.py
+++ Software/Regression Code/test_one.py
@@ -1,9 +1,31 @@
-import math 
+import math
+
+import cantools.database 
 from CANBase import CANInterface
+
+import cantools
+import os
+dbc_directory = os.path.join(os.getcwd(), 'Software\DBC Tools\output.dbc')
+dbc_file = dbc_directory #'..\DBC Tools\output.dbc'
+db = cantools.database.load_file(dbc_file)
+
+message = db.get_message_by_name('HARDINJ_command')
+
+data = message.encode({
+    'HARDINJ_mux': 0,
+    'HARDINJ_output1Control': 127,
+    'HARDINJ_output2Control': 200,
+    'HARDINJ_output3Control': 200,
+    'HARDINJ_output4Control': 200,
+    'HARDINJ_output5Control': 200,
+    'HARDINJ_output6Control': 200,
+
+})
+
 
 def startCAN():
     can_interface = CANInterface()
-    can_interface.send_can_message(can_id=0xA11, data=[0x11, 0x22, 0x33], is_extended=False)
+    can_interface.send_can_message(can_id=0x001, data=data, is_extended=False)
     can_interface.__del__()
     print("test")
     assert(True==True)
@@ -13,4 +35,5 @@ def test_sqrt():
     assert math.sqrt(25) == 5
 
 def test_can():
+    print(data)
     startCAN()
